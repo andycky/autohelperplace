90cdbaf75f0fa154c31856c245137dde08aab600
andy.cheng <andy.cheng@uatlivibank.com>
2022-08-04 22:24:40 +0800

    make it better

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def get_chrome():
     case = 0
 
     config = configparser.ConfigParser()
-    config.read('config/config.properties')
+    config.read('../config/config.properties')
 
     user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
 
